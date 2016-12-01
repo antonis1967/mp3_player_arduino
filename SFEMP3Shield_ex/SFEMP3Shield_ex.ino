6a2193e8a16d5d8177a84cbf53adc1c3d78db644
Bill Dufresne <371570273@qq.com>
2016-11-30 23:38:51 -0600

    play 2 songs ok

--- SFEMP3Shield_ex/SFEMP3Shield_ex.ino
+++ SFEMP3Shield_ex/SFEMP3Shield_ex.ino
@@ -13,20 +13,17 @@ void setup() {
   //start the shield
   sd.begin(SD_SEL, SPI_HALF_SPEED);
   MP3player.begin();
-
-  //start playing track 1
-  //MP3player.playTrack(1);
 }
 
 //do something else now
 void loop() {
 
   Serial.println("I'm bored!");
-  delay(2000);
+
+  //start playing track 1
   MP3player.playTrack(1);
-
+  delay(2000);
+  
   MP3player.playTrack(2);
-
-  MP3player.playTrack(3);
-
+  delay(2000);
 }
